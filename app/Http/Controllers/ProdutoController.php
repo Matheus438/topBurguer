b4c360c1f33650b4e7c9756b41e0b4c2ea8e1a99
Matheus438 <matheusbispoa3@gmail.com>
2024-04-04 15:52:29 -0300

    comimit

--- app/Http/Controllers/ProdutoController.php
+++ app/Http/Controllers/ProdutoController.php
@@ -14,10 +14,24 @@ class ProdutoController extends Controller
                 'nome' => $produto->nome,
                 'preco' => $produto->preco,
                 'ingredientes' => $produto->ingredientes,
-                'image' => asset('storage/' . $produto->imagem)
+                'imagem' => asset('storage/' . $produto->imagem)
 
             ];
         });
         return response()->json($produtosComImagem);
+    }
+
+    public function store(Request $request){
+        $produtoData = $request->all();
+        
+        if($request->hasFile('imagem')){
+            $imagem = $request->file('imagem');
+            $nomeImagem = time().'.'.$imagem->getClientOriginalExtension();
+            $caminhoImagem = $imagem->storeAs('imagem/produtos', $nomeImagem, 'public');
+            $produtoData['imagem'] = $caminhoImagem;
+        }
+
+        $produto = Produto::create($produtoData);
+        return response()->json(['produto'=>$produto], 201);
     }
 }
